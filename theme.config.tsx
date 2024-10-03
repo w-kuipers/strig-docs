aa4aea522ff609048bfd90c15aca5da165fd2e0a
Wibo Kuipers <wibokuip@gmail.com>
2024-10-03 20:58:23 +0200

    Update theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,10 +3,20 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
 	logo: <h2 style={{ fontWeight: "bolder" }}>Strig</h2>,
+	project: {
+		link: "https://github.com/w-kuipers/strig",
+	},
+	useNextSeoProps() {
+		return {
+			titleTemplate: "%s – Strig",
+		};
+	},
 	docsRepositoryBase: 'https://github.com/w-kuipers/strig-docs',
 	footer: {
 		text: 'A simple and intuitive Python package for generating random strings and numbers.',
 	},
+	primaryHue: 30,
+	primarySaturation: 50
 }
 
 export default config
