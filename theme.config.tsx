82eb4d4aab6ddb79296ad9875ce0107c73d73771
Wibo Kuipers <wibokuip@gmail.com>
2024-10-03 20:11:09 +0200

    Change introduction

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <h2 style={{ fontWeight: "bolder" }}>Simpleuid</h2>,
-  docsRepositoryBase: 'https://github.com/w-kuipers/simpleuid-docs',
-  footer: {
-    text: 'Random ID generation in Python',
-  },
+	logo: <h2 style={{ fontWeight: "bolder" }}>Strig</h2>,
+	docsRepositoryBase: 'https://github.com/w-kuipers/strig-docs',
+	footer: {
+		text: 'A simple and intuitive Python package for generating random strings and numbers.',
+	},
 }
 
 export default config
