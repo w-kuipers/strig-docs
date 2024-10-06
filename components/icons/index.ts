5bb8b076eed4e2a90863c6ab4aeeaf3f32f53811
Wibo Kuipers <wibokuip@gmail.com>
2024-10-06 13:46:16 +0200

    Add var docs

---
+++ components/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as PlusIcon } from './plus.svg'
+export { default as Calendar } from './calendar.svg'
+export { default as System } from './system.svg'
--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+	theme: 'nextra-theme-docs',
+	themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+	webpack(config) {
+		const allowedSvgRegex = /components\/icons\/.+\.svg$/
+
+		const fileLoaderRule = config.module.rules.find(rule =>
+			rule.test?.test?.('.svg')
+		)
+		fileLoaderRule.exclude = allowedSvgRegex
+
+		config.module.rules.push({
+			test: allowedSvgRegex,
+			use: ['@svgr/webpack']
+		})
+	}
+});
